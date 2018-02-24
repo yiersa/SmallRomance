604487c3aeed2284fc6ac2aa9ef3e3b945a699fe
taomingwang <taojike@126.com>
2018-02-24 18:08:59 +0800

    文案调整

--- src/config/util.js
+++ src/config/util.js
@@ -14,7 +14,15 @@ const util = {
 	//ms => 帧
 	transTime(time, defult){
 		return +time / 1000 * 60 | 0 || defult;
-	}
+	},
+    GetDateDiff() {
+    	var date1= '2015/03/08 00:00:00';  //开始时间
+    	var date2 = new Date();    //结束时间
+    	var date3 = date2.getTime() - new Date(date1).getTime();   //时间差的毫秒数
+    	//计算出相差天数
+    	var days=Math.floor(date3/(24*3600*1000))
+        return days;
+    }
 }
 
 export default util
